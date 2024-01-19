76d51e22279b61496f6795d9016bf7bedf83ece5
liutyinazar <lyutiy.n@gmail.com>
2024-01-20 01:22:00 +0300

    v0.1.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 

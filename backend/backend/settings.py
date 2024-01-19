af98a2db9a8e641be3d6550fab957a50540802f8
liutyinazar <lyutiy.n@gmail.com>
2024-01-20 01:44:36 +0300

    v1_fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "root",
         "HOST": "db",
         "PORT": 5432,
     }
